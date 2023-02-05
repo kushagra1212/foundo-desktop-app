3296d51e2c77c6ac9f71d017af6c0cc7d1be792e
unknown <kushagrarathore002@gmail.com>
2023-02-06 00:43:42 +0530

    :construction_worker: UI ADD

--- src/app/home/feed/feed.module.ts
+++ src/app/home/feed/feed.module.ts
@@ -7,6 +7,7 @@ import { PostListComponent } from './components/post-list/post-list.component';
 import { SearchHeaderComponent } from './components/search-header/search-header.component';
 import { MatIconModule } from '@angular/material/icon';
 import { NavComponent } from './components/nav/nav.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,8 @@ import { NavComponent } from './components/nav/nav.component';
     PostListComponent,
     SearchHeaderComponent,
     NavComponent,
+    CardComponent,
+    CardComponent,
   ],
   imports: [CommonModule, FeedRoutingModule, SharedModule, MatIconModule],
   exports: [],
